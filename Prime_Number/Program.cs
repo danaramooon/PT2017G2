02f001fb9b887bff56fdb4805f330ef3eef74557
Your Name <you@example.com>
2017-01-24 22:46:34 +0600

    added comments

--- Prime_Number/Program.cs
+++ Prime_Number/Program.cs
@@ -8,13 +8,17 @@ namespace Prime_Number
 {
     class Program
     {
+        // function for checking a number to determine whether a number is a prime or not
         static bool check(int n)
         {
+            //to define whether 'x' is divided into 'i'
             for (int i = 2; i < n; i++)
             {
                 if (n % i == 0)
+                    //it is not a prime number,a program has to finish cycle
                     return false;
             }
+            // '1' is not a prime number
             if (n == 1)
                 return false;
             return true;
@@ -22,15 +26,22 @@ namespace Prime_Number
 
         public static void Main(string[] args)
         {
+            //enter data
             string a = Console.ReadLine();
+            //rewrite elements from string a to array arr
             string[] arr = a.Split();
             for(int i = 0; i < arr.Length; i++ )
             {
+                //to assign each element of the array to a string
                 string s = arr[i];
+                //to convert string to integer
                 int p = int.Parse(s);
+                //check it whether it is a prime number or not
                 if (check(p))
+                    //display it
                     Console.WriteLine(p + " ");
             }
+            //program will not finish until we press any key
             Console.ReadKey();
         }
     }
