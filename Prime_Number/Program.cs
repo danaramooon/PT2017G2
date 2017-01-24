6b74bdd8ab2be6b834240923f1bc9b5eb0d897e8
Your Name <you@example.com>
2017-01-24 21:55:38 +0600

    determination of prime number

---
+++ Prime_Number/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Number
+{
+    class Program
+    {
+        static bool check(int n)
+        {
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            if (n == 1)
+                return false;
+            return true;
+        }
+
+        public static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string[] arr = a.Split();
+            for(int i = 0; i < arr.Length; i++ )
+            {
+                string s = arr[i];
+                int p = int.Parse(s);
+                if (check(p))
+                    Console.WriteLine(p + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
